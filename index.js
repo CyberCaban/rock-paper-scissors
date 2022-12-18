2b86faaf168f5160839722454f7f519d10e1d313
CyberCaban <andrewgamer63@gmail.com>
2022-12-18 20:45:40 +0300

    more additions

--- index.js
+++ index.js
@@ -7,27 +7,31 @@ const io = new Server(server);
 
 let room;
 
+app.use(express.static(__dirname + '/'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+    //отправка ID юзеру
+    io.emit('send user id', socket.id)
+
     //присоединяем юзера к комнате
     socket.on('join room', (roomID)=>{
         room = roomID
         socket.join(roomID);
+
         //отправка ID комнаты юзеру
         io.emit('current room', roomID)
         console.log(roomID)
+        console.log(socket.id)
     })
+
     //получение сообщения сервером и отправка их юзеру
     socket.on('send message', (message)=>{
-        io.to(room).emit('getMessage', message)
+        io.in(room).emit('getMessage', message)
     })
 });
-
-
-
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
